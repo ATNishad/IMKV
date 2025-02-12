ae319a43625f07860fb5947963b3de9da8908090
Nishad <thanveer2893@gmail.com>
2025-02-12 19:32:19 +0530

    Refactored server code, removed error checks

--- server.cpp
+++ server.cpp
@@ -2,109 +2,56 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
-#pragma comment(lib,"ws2_32")   //supported only in clang or MSVC
+#pragma comment(lib,"ws2_32") //for clang or mvsc
 
-//replace ip and port with preferred server ip and port
-#define SERVER_PORT_NUM 5000        
-#define SERVER_IP_ADDR "127.0.0.1"  
+#define CLIENT_PORT_NUM 5000
+#define CLIENT_IP_ADDR "127.0.0.1"
 
 
 #include<winsock2.h>
-#include<ws2tcpip.h>
+#include<windows.h>
 #include<iostream>
 
-    
+
 int main(){
+    //wsa startup
+    WSADATA wsadata;
+    WSAStartup(MAKEWORD(2,2), &wsadata);
     
-    //winsock initialization
-    WSADATA wsadata;  
-    int iresult;
-
-    iresult = WSAStartup(MAKEWORD(2,2),&wsadata);
-    if( iresult != 0){
-        std::cout<<"WSASTARTUP FAILED:"<<WSAGetLastError()<<"\n";
-        return -1;
-    }
-
-
-    //server socket creation
+    //socket init
     int optval = 1;
-    SOCKET socket_handle = socket(AF_INET,SOCK_STREAM, 0);
-    if(socket_handle == INVALID_SOCKET){
-        std::cout<<"SOCKET INITIALIZATION FAILED:"<<WSAGetLastError<<"\n";
-        return -1;
-    }
-    else{
-    if(setsockopt(socket_handle,SOL_SOCKET,SO_REUSEADDR,(const char*)(&optval),sizeof(optval)) !- 0){
-        std::cout<<"SET SOCKET OPTION FAILED:"<<WSAGetLastError<<"\n";
-        return -1;
-    }
-    }
+    SOCKET server_socket_handle = socket(AF_INET,SOCK_STREAM,0);
+    setsockopt(server_socket_handle,SOL_SOCKET,SO_REUSEADDR,(const char*)&optval,sizeof(optval));
     
-    //binding socket to port and ip
-    sockaddr_in socketAddressBind = {};
-    socketAddressBind.sin_family = AF_INET; 
-    socketAddressBind.sin_port = htons(SERVER_PORT_NUM);
-    inet_pton(AF_INET,SERVER_IP_ADDR,&socketAddressBind.sin_addr.s_addr);
+    //socket bind
+    sockaddr_in serverSocketAddressBind = {};
+    serverSocketAddressBind.sin_family = AF_INET;
+    serverSocketAddressBind.sin_port = htons(CLIENT_PORT_NUM);
+    serverSocketAddressBind.sin_addr.s_addr = inet_addr(CLIENT_IP_ADDR);
+    bind(server_socket_handle,(const sockaddr*)&serverSocketAddressBind,sizeof(serverSocketAddressBind));
 
-    int bindVal = bind(socket_handle,(const sockaddr*)&socketAddressBind,sizeof(socketAddressBind));
-    if( bindVal != 0){
-        std::cout<<"BIND FAILED:"<<WSAGetLastError()<<"\n";
-        return -1;
-    }
 
+    //socket listen
+    listen(server_socket_handle,SOMAXCONN);
+    //socket accept
+    sockaddr_in connecting_socket = {};
+    int cs_len = sizeof(connecting_socket);
+    SOCKET new_comm_socket = accept(server_socket_handle,(sockaddr*)&connecting_socket,&cs_len);
     
-    //set socket to listen 
-    if(listen(socket_handle,SOMAXCONN) == SOCKET_ERROR){
-        std::cout<<"LISTEN FAILED:"<<WSAGetLastError()<<"\n";
-        return -1;
-    }
-
-    //loop for the socket to accept client socket
     while(true){
-    std::cout<<"Waiting for connection..."<<"\n";
-    sockaddr_in client_sock_addr = {};
-    int client_sa_len = sizeof(client_sock_addr);
-    SOCKET acceptedSocket = accept(socket_handle,(sockaddr*)&client_sock_addr, &client_sa_len);
-    if( acceptedSocket == INVALID_SOCKET){
-        std::cout<<"ACCEPT FAILED:"<<WSAGetLastError()<<"\n";
-        continue;
+    std::string s_server_msg = "HELLO FROM SERVER";
+    std::string r_client_msg(1024, '\0');
+
+    send(new_comm_socket,s_server_msg.c_str(),s_server_msg.size(),0);
+    size_t recv_byte_size = recv(new_comm_socket,&r_client_msg[0],r_client_msg.size(),0);
+    r_client_msg.resize(recv_byte_size);
+    std::cout<<r_client_msg<<"\n";
+    Sleep(10000);
+    break;
     }
 
-    //on successful acceptance send()
-    else{
-    std::cout<<"CLIENT CONNECTED!!"<<"\n";
-    
-    std::string send_msg = "hello from server";
-    std::string recv_msg(1024 , '\0');
-
-    if(send(acceptedSocket,send_msg.c_str(),send_msg.size(),0)== SOCKET_ERROR){
-        std::cout<<"SEND FAILED:"<<WSAGetLastError()<<"\n";
-        return -1;
-    }
-
-    if(recv(acceptedSocket,&recv_msg[0],recv_msg.size(),0) == SOCKET_ERROR){
-        std::cout<<"RECEIVE FAILED:"<<WSAGetLastError<<"\n";
-        return -1;
-    }
-    else{
-        std::cout<<recv_msg;
-    }
-
-    }
-
-
-    }
-
-
-    //close socket at end
-    if(closesocket(socket_handle) != 0){
-        std::cout<<"SOCKET FAILED TO CLOSE:"<< WSAGetLastError()<<"\n";
-        return -1;
-    }
-    else{
-        std::cout<<"SOCKET CLOSED"<<"\n";
-        WSACleanup();
-    }
+    shutdown(new_comm_socket,SD_BOTH);
+    closesocket(new_comm_socket);
+    closesocket(server_socket_handle);    
+    WSACleanup();
 }
-
