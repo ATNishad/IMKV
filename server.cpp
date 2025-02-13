d9ef652949390c75d1755c3a2cac6459de6eeb7a
Nishad <thanveer2893@gmail.com>
2025-02-13 19:22:02 +0530

    application protocol integrated with server.cpp (untested)

--- server.cpp
+++ server.cpp
@@ -7,11 +7,89 @@
 #define CLIENT_PORT_NUM 5000
 #define CLIENT_IP_ADDR "127.0.0.1"
 
-
 #include<winsock2.h>
 #include<windows.h>
 #include<iostream>
+#include<cassert>
+#include<cstdint>
 
+
+namespace full_SR{
+
+    int receive_full(SOCKET handle,char* receive_buffer,size_t r_buffer_size){
+        while( r_buffer_size > 0 ){ 
+        ssize_t recv_size = recv(handle,receive_buffer,r_buffer_size,0);
+        if(recv_size <= 0){
+            return -1;  //receive error
+        }
+        assert(size_t(recv_size) < r_buffer_size); //check size 
+        r_buffer_size -= size_t(recv_size);
+        receive_buffer += recv_size;
+        }
+        return 0;
+    }
+    
+    int send_full(SOCKET handle,char* send_buffer,size_t s_buffer_size){
+        while( s_buffer_size > 0){
+        ssize_t send_size = send(handle,send_buffer,s_buffer_size,0);
+        if(send_size <= 0){
+            return -1;
+        }
+        assert((size_t)send_size < s_buffer_size);
+        s_buffer_size -= size_t(send_size);
+        send_buffer += send_size;
+        }
+        return 0;
+    }
+    
+}
+
+int application_protocol(SOCKET new_comm_socket){
+    const size_t MAX_MSG_SIZE=2096; 
+    
+    //RECEIVE
+    char receive_buffer[4+ MAX_MSG_SIZE] = {};
+    size_t r_buffer_size = sizeof(receive_buffer);
+
+    //receiving first 4 bytes
+    int r_err = full_SR::receive_full(new_comm_socket,receive_buffer,4);
+    if(r_err != 0){
+        std::cerr<<"FULL RECEIVE FAILED (byte)\n";
+        return -1;        
+    }
+    //storing the 4 bytes
+    uint32_t r_len = 0;
+    memcpy(&r_len,receive_buffer,4);
+    if(r_len > MAX_MSG_SIZE){
+        std::cerr<<"RECEIVED MSG EXCEEDS BUFFER SIZE\n";
+    }
+
+    //receiving the actual message
+    r_err = full_SR::receive_full(new_comm_socket,&receive_buffer[4],r_len);
+    if(r_err != 0){
+        std::cerr<<"FULL RECEIVE FAILED (msg)\n";
+    }
+    //printing client message on server    
+    std::cout<<"RECEIVED FROM CLIENT:"<<&receive_buffer[4];
+    
+    //SEND
+    char msg[] = "HELLO FROM SERVER";
+    char send_buffer[4+sizeof(msg)];
+    
+    uint32_t s_len = sizeof(msg);
+    memcpy(send_buffer,&s_len,4);
+    memcpy(&send_buffer[4],msg,s_len);
+
+    
+    int s_err = full_SR::send_full(new_comm_socket,send_buffer,4+s_len);
+    if(s_err == -1){
+        std::cerr<<"FULL SEND FAILED\n";
+        return -1;        
+    }
+
+
+    return 0;
+}
 
 int main(){
     //wsa startup
@@ -33,21 +111,30 @@ int main(){
 
     //socket listen
     listen(server_socket_handle,SOMAXCONN);
+    std::cout<<"LISTENING FOR CONNECTION...\n";
     //socket accept
+    SOCKET new_comm_socket ={};
+
+    while(true){
     sockaddr_in connecting_socket = {};
     int cs_len = sizeof(connecting_socket);
-    SOCKET new_comm_socket = accept(server_socket_handle,(sockaddr*)&connecting_socket,&cs_len);
-    
-    while(true){
-    char s_server_msg[] = "HELLO FROM SERVER";
-    char r_client_msg[1024] = {};
-
-    send(new_comm_socket,s_server_msg,sizeof(s_server_msg)-1,0);
-    size_t recv_byte_size = recv(new_comm_socket,&r_client_msg[0],sizeof(r_client_msg)-1,0);
-    std::cout<<r_client_msg<<"\n";
-    Sleep(10000);
-    break;
+    new_comm_socket = accept(server_socket_handle,(sockaddr*)&connecting_socket,&cs_len);
+    if(new_comm_socket < 0){
+        closesocket(new_comm_socket);
+        continue;
     }
+    std::cout<<"CLIENT CONNECTED\n";
+
+    while(true){
+    int err = application_protocol(new_comm_socket);
+    if(err != 0){
+        std::cerr<<"APPLICATION PROTOCOL ERROR\n";
+        break;
+    }
+    }
+    break;
+    } 
+
 
     shutdown(new_comm_socket,SD_BOTH);
     closesocket(new_comm_socket);
