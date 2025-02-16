c0263c47a9820282f4972b2b62b090e9dc66b472
Nishad <thanveer2893@gmail.com>
2025-02-16 20:33:53 +0530

    minor size fixes

--- client.cpp
+++ client.cpp
@@ -1,51 +1,48 @@
-    #ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
-    #endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 
-    #pragma comment(lib,"ws2_32") //for clang or mvsc
+#pragma comment(lib,"ws2_32")
 
-    #define CLIENT_PORT_NUM 5000
-    #define CLIENT_IP_ADDR "127.0.0.1"
+#define CLIENT_PORT_NUM 5000
+#define CLIENT_IP_ADDR "127.0.0.1"
 
-    #include<winsock2.h>
-    #include<windows.h>
-    #include<iostream>
-    #include<cassert>
-    #include<cstdint>
+#include<winsock2.h>
+#include<windows.h>
+#include<iostream>
+#include<cassert>
+#include<cstdint>
 
-    namespace full_SR{
+namespace full_SR{
 
-        int receive_full(SOCKET handle,char* receive_buffer,size_t r_buffer_size){
-            while( r_buffer_size > 0 ){ 
-            ssize_t recv_size = recv(handle,receive_buffer,r_buffer_size,0);
-            if(recv_size <= 0){
-                return -1;  //receive error
-            }
-            assert(size_t(recv_size) <= r_buffer_size); //check size 
-            r_buffer_size -= size_t(recv_size);
-            receive_buffer += recv_size;
-            }
-            return 0;
+    int receive_full(SOCKET handle,char* receive_buffer,size_t r_buffer_size){
+        while( r_buffer_size > 0 ){ 
+        int recv_size = recv(handle,receive_buffer,r_buffer_size,0);
+        if(recv_size <= 0){
+            return -1;
         }
-        
-        int send_full(SOCKET handle,char* send_buffer,size_t s_buffer_size){
-            while( s_buffer_size > 0){
-            ssize_t send_size = send(handle,send_buffer,s_buffer_size,0);
-            if(send_size <= 0){
-                return -1;
-            }
-            assert((size_t)send_size <= s_buffer_size);
-            s_buffer_size -= size_t(send_size);
-            send_buffer += send_size;
-            }
-            return 0;
+        assert(size_t(recv_size) <= r_buffer_size);
+        r_buffer_size -= size_t(recv_size);
+        receive_buffer += recv_size;
         }
-        
+        return 0;
     }
-
+    
+    int send_full(SOCKET handle,char* send_buffer,size_t s_buffer_size){
+        while( s_buffer_size > 0){
+        int send_size = send(handle,send_buffer,s_buffer_size,0);
+        if(send_size <= 0){
+            return -1;
+        }
+        assert((size_t)send_size <= s_buffer_size);
+        s_buffer_size -= size_t(send_size);
+        send_buffer += send_size;
+        }
+        return 0;
+    }
+}
 
 int32_t client_protocol(SOCKET socket, char* msg){
-
     const size_t MAX_MSG_SIZE= 2096;
 
     uint32_t s_len = (uint32_t)strlen(msg);
@@ -63,8 +60,6 @@ int32_t client_protocol(SOCKET socket, char* msg){
     }
 
     char receive_buffer[4+MAX_MSG_SIZE] = {};
-    size_t r_buffer_size = sizeof(receive_buffer);
-
     uint32_t r_err = full_SR::receive_full(socket,receive_buffer,4);
     if(r_err != 0){
         std::cerr<<"FULL RECEIVE FAILED (byte)\n";
@@ -85,37 +80,34 @@ int32_t client_protocol(SOCKET socket, char* msg){
     }
 
     std::cout<<"RECEIVED FROM SERVER:"<<&receive_buffer[4]<<"\n";
-        return 0;
-    }
+    return 0;
+}
 
+int main(){
+    WSADATA wsadata;
+    WSAStartup(MAKEWORD(2,2), &wsadata);
 
-
-    int main(){
-        WSADATA wsadata;
-        WSAStartup(MAKEWORD(2,2), &wsadata);
-
-        int optval = 1;
-        SOCKET client_socket_handle = socket(AF_INET,SOCK_STREAM,0);
-        setsockopt(client_socket_handle,SOL_SOCKET,SO_REUSEADDR,(const char*)&optval,sizeof(optval));
-        
-        sockaddr_in clientSocketAddressBind = {};
-        clientSocketAddressBind.sin_family = AF_INET;
-        clientSocketAddressBind.sin_port = htons(CLIENT_PORT_NUM);
-        clientSocketAddressBind.sin_addr.s_addr = inet_addr(CLIENT_IP_ADDR);
-
-
-        connect(client_socket_handle,(const sockaddr*)&clientSocketAddressBind,sizeof(clientSocketAddressBind));
-
-        char msg[] = "HELLO FROM CLIENT";
-        int32_t err = client_protocol(client_socket_handle,msg);
-        if(err != 0){
-            std::cerr<<"CLIENT PROTOCOL ERROR: server offline\n";
-            goto c_close;
-        }
-
-        std::cin.get();
+    int optval = 1;
+    SOCKET client_socket_handle = socket(AF_INET,SOCK_STREAM,0);
+    setsockopt(client_socket_handle,SOL_SOCKET,SO_REUSEADDR,(const char*)&optval,sizeof(optval));
     
-        c_close:
-        closesocket(client_socket_handle);
-        WSACleanup();
+    sockaddr_in clientSocketAddressBind = {};
+    clientSocketAddressBind.sin_family = AF_INET;
+    clientSocketAddressBind.sin_port = htons(CLIENT_PORT_NUM);
+    clientSocketAddressBind.sin_addr.s_addr = inet_addr(CLIENT_IP_ADDR);
+
+    connect(client_socket_handle,(const sockaddr*)&clientSocketAddressBind,sizeof(clientSocketAddressBind));
+
+    char msg[] = "HELLO FROM CLIENT";
+    int32_t err = client_protocol(client_socket_handle,msg);
+    if(err != 0){
+        std::cerr<<"CLIENT_PROTOCOL_ERROR: Server Offline\n";
+        goto c_close;
     }
+
+    std::cin.get();
+
+c_close:
+    closesocket(client_socket_handle);
+    WSACleanup();
+}
--- server.cpp
+++ server.cpp
@@ -2,7 +2,7 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
-#pragma comment(lib,"ws2_32") //for clang or mvsc
+#pragma comment(lib,"ws2_32")
 
 #define CLIENT_PORT_NUM 5000
 #define CLIENT_IP_ADDR "127.0.0.1"
@@ -13,17 +13,15 @@
 #include<cassert>
 #include<cstdint>
 
-
-
 namespace full_SR{
 
     int receive_full(SOCKET handle,char* receive_buffer,size_t r_buffer_size){
         while( r_buffer_size > 0 ){ 
-        ssize_t recv_size = recv(handle,receive_buffer,r_buffer_size,0);
+        int recv_size = recv(handle,receive_buffer,r_buffer_size,0);
         if(recv_size <= 0){
-            return -1;  //receive error
+            return -1;
         }
-        assert(size_t(recv_size) <= r_buffer_size); //check size 
+        assert(size_t(recv_size) <= r_buffer_size);
         r_buffer_size -= size_t(recv_size);
         receive_buffer += recv_size;
         }
@@ -32,7 +30,7 @@ namespace full_SR{
     
     int send_full(SOCKET handle,char* send_buffer,size_t s_buffer_size){
         while( s_buffer_size > 0){
-        ssize_t send_size = send(handle,send_buffer,s_buffer_size,0);
+        int send_size = send(handle,send_buffer,s_buffer_size,0);
         if(send_size <= 0){
             return -1;
         }
@@ -48,17 +46,14 @@ namespace full_SR{
 int32_t server_protocol(SOCKET new_comm_socket){
     const size_t MAX_MSG_SIZE=2096; 
     
-    //RECEIVE
     char receive_buffer[4+ MAX_MSG_SIZE] = {};
-    size_t r_buffer_size = sizeof(receive_buffer);
-
-    //receiving first 4 bytes
+    
     int32_t r_err = full_SR::receive_full(new_comm_socket,receive_buffer,4);
     if(r_err != 0){
         std::cerr<<"FULL RECEIVE FAILED (byte)\n";
         return -1;        
     }
-    //storing the 4 bytes
+    
     uint32_t r_len = 0;
     memcpy(&r_len,receive_buffer,4);
     if(r_len > MAX_MSG_SIZE){
@@ -66,16 +61,14 @@ int32_t server_protocol(SOCKET new_comm_socket){
         return -1;
     }
 
-    //receiving the actual message
     r_err = full_SR::receive_full(new_comm_socket,&receive_buffer[4],r_len);
     if(r_err != 0){
         std::cerr<<"FULL RECEIVE FAILED (msg)\n";
         return -1;
     }
-    //printing client message on server    
+    
     std::cout<<"RECEIVED FROM CLIENT:"<<&receive_buffer[4]<<"\n";
     
-    //SEND
     char msg[] = "HELLO FROM SERVER";
     char send_buffer[4+sizeof(msg)];
     
@@ -83,7 +76,6 @@ int32_t server_protocol(SOCKET new_comm_socket){
     memcpy(send_buffer,&s_len,4);
     memcpy(&send_buffer[4],msg,s_len);
 
-    
     int s_err = full_SR::send_full(new_comm_socket,send_buffer,4+s_len);
     if(s_err == -1){
         std::cerr<<"FULL SEND FAILED\n";
@@ -94,44 +86,36 @@ int32_t server_protocol(SOCKET new_comm_socket){
 }
 
 int main(){
-    //wsa startup
     WSADATA wsadata;
     WSAStartup(MAKEWORD(2,2), &wsadata);
     
-    //socket init
     int optval = 1;
     SOCKET server_socket_handle = socket(AF_INET,SOCK_STREAM,0);
     setsockopt(server_socket_handle,SOL_SOCKET,SO_REUSEADDR,(const char*)&optval,sizeof(optval));
     
-    //socket bind
     sockaddr_in serverSocketAddressBind = {};
     serverSocketAddressBind.sin_family = AF_INET;
     serverSocketAddressBind.sin_port = htons(CLIENT_PORT_NUM);
     serverSocketAddressBind.sin_addr.s_addr = inet_addr(CLIENT_IP_ADDR);
     bind(server_socket_handle,(const sockaddr*)&serverSocketAddressBind,sizeof(serverSocketAddressBind));
 
-
-    //socket listen
     listen(server_socket_handle,SOMAXCONN);
     std::cout<<"LISTENING FOR CONNECTION...\n";
-    //socket accept
     SOCKET new_comm_socket ={};
 
     while(true){
-
     sockaddr_in connecting_socket = {};
     int cs_len = sizeof(connecting_socket);
     new_comm_socket = accept(server_socket_handle,(sockaddr*)&connecting_socket,&cs_len);
-    if(new_comm_socket < 0){
+    if(new_comm_socket == INVALID_SOCKET){
         closesocket(new_comm_socket);
         continue;
     }
-    std::cout<<"CLIENT CONNECTED\n";
 
     while(true){
     int32_t err = server_protocol(new_comm_socket);
     if(err != 0){
-        std::cerr<<"APPLICATION PROTOCOL ERROR\n";
+        std::cerr<<"APPLICATION_PROTOCOL_ERROR : Client Offline\n";
         break;
     }
     }
@@ -144,5 +128,4 @@ int main(){
     closesocket(new_comm_socket);
     closesocket(server_socket_handle);    
     WSACleanup();
-
 }
